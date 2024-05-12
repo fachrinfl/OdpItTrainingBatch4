dc8812c2d182fa3ed0a4d268a17b2aa9b47c6aa6
Fachri Naufal <fachrinaufal@Fachris-MacBook-Pro.local>
2024-05-12 23:22:39 +0700

    integrate sign-in firebase with login screen

--- src/features/auth/screen/LoginScreen.js
+++ src/features/auth/screen/LoginScreen.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import {View, StyleSheet, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, TextInput, Button, Alert} from 'react-native';
 import {navigationRef} from '../../../navigation/navigationUtils';
+import auth from '@react-native-firebase/auth';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      if (email.length && password.length) {
+        await auth().signInWithEmailAndPassword(email, password);
+        navigationRef.reset({
+          index: 0,
+          routes: [{name: 'LoginScreen'}],
+        });
+      } else {
+        Alert.alert('Invalid credential', 'Your email & password is invalid', [
+          {text: 'OK', onPress: () => {}},
+        ]);
+      }
+    } catch (error) {
+      console.error(error);
+      if (error.code) {
+        Alert.alert(error.code, error.message, [
+          {text: 'OK', onPress: () => {}},
+        ]);
+      }
+    }
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>Login screen</Text>
-      <Button
-        title="Register"
-        onPress={() => navigationRef.navigate('RegisterScreen')}
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
       />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingLeft: 10,
   },
 });
 

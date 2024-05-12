677b102bd33771b0b5898c8332b9ea544d33648d
Fachri Naufal <fachrinaufal@Fachris-MacBook-Pro.local>
2024-05-13 01:36:30 +0700

    add theme

---
+++ android/app/src/theme/typography.js
@@ -0,0 +1,48 @@
+const typography = {
+  h1: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 28,
+  },
+  h2: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 24,
+  },
+  h3: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 22,
+  },
+  h4: {
+    fontFamily: 'Lato-Light',
+    fontSize: 20,
+  },
+  h5: {
+    fontFamily: 'Lato-Light',
+    fontSize: 18,
+  },
+  h6: {
+    fontFamily: 'Lato-Light',
+    fontSize: 16,
+  },
+  h7: {
+    fontFamily: 'Lato-Light',
+    fontSize: 14,
+  },
+  paragraph18: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 18,
+  },
+  paragraph16: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 16,
+  },
+  paragraph14: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 14,
+  },
+  paragraph12: {
+    fontFamily: 'Lato-Regular',
+    fontSize: 12,
+  },
+};
+
+export default typography;
